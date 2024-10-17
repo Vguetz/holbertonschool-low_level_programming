7345ff9d759a4525dd9c28da70cfaec8b3918db1
Vguetz <gomezlucas712@gmail.com>
2024-10-17 16:28:57 +0000

    6

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - function main
+* Return: return 0
+*/
+int main(void)
+{
+printf("Size of a char: %u", sizeof(char));
+printf("Size of an int: %u", sizeof(int));
+pritnf("Size of a long int: %u", sizeof(long int));
+pritnf("Size of a long long int: %u", sizeof(long long int));
+pritnf("Size of a float: %u", sizeof(float));
+return (0);
+}
