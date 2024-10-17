02a81087c2b02bfa8af66fa8a0d4cbeed36ddf32
Vguetz <gomezlucas712@gmail.com>
2024-10-17 15:53:07 +0000

    5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
-int main() {
-	puts("Programming is like building a multilingual puzzle");
-	
-	return 0;
+/**
+* main - function
+* Return: this function return 0
+*
+*
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art, ");
+return (0);
 }
