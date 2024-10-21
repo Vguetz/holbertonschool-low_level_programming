0d9e5bc850fbebbb808a70c09b7f6e60e6077c3d
Vguetz <gomezlucas712@gmail.com>
2024-10-21 13:22:11 +0000

    add

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * _putchar - function
  * @c: texto a imprimir
@@ -8,5 +8,5 @@
  */
 int _putchar(char c)
 {
-	return (putchar(c));
+	return (write(1, &c, 1));
 }
