fce6eb8bdfa6b28b9aae128a42c07b650f43a9da
Vguetz <gomezlucas712@gmail.com>
2024-10-23 13:33:10 +0000

    0

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,19 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
-/**
- * print_digit - Helper
- * @n: number to print
- */
-void print_digit(int n)
-{
-	if (n / 10)
-	{
-		print_digit(n / 10);
-	}
-	_putchar((n % 10) + '0');
-}
-
 /**
  * _abs - function to compute the absolute value of an integer and print it
  * @n: the integer to check
