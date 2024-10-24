017815e317d0501539b35d2fc9fd838c4f439e37
Vguetz <gomezlucas712@gmail.com>
2024-10-24 14:33:50 +0000

    fisrtrie10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - function sum
+ * @a: first number to sum
+ * @b: second number to sum
+ * 
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+	return (result);
+}
