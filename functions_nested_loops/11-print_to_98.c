4f56a56346d6ebd474f08d1367ab29b7bd09530c
Vguetz <gomezlucas712@gmail.com>
2024-10-25 13:26:30 +0000

    11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -31,7 +31,7 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		while (n <= 98)
+		while (n < 98)
 		{
 			print_number(n);
 			_putchar(',');
@@ -40,7 +40,7 @@ void print_to_98(int n)
 		}
 	} else if (n > 98)
 	{
-		while (n >= 98)
+		while (n > 98)
 		{
 			print_number(n);
 			_putchar(',');
