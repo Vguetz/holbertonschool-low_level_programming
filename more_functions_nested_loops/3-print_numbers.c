cae2a4dc26f797d908e7531a06a293a72a257a8a
Vguetz <gomezlucas712@gmail.com>
2024-10-28 13:14:37 +0000

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+void print_numbers(void)
+{
+	int i =0;
+
+	while(i <= 9)
+	{
+		_putchar(i + 48);
+		i++;
+	}
+	_putchar('\n');
+}
---
+++ more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * _putchar - function
+ * @c: texto a imprimir
+ *
+ * Return: void
+ *
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
