0b39bd18d66e6677e1a7001be101b5c9baacffd7
Vguetz <gomezlucas712@gmail.com>
2024-10-28 13:06:03 +0000

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multplies 2 numbers
+ * @a: first number of mul
+ * @b: second number of mul
+ *
+ * Return: product of a * b
+ */
+int mul(int a, int b)
+{
+	return a * b;
+}
