9f5d1c70138158e0b6a70462df99375e0f86a043
Vguetz <gomezlucas712@gmail.com>
2024-10-29 12:58:56 +0000

    10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of a triangle to print
+ *
+ * Return: void
+ *
+ */
+
+void print_triangle(int size)
+{
+	int i = 1, j;
+
+	while (i <= size)
+	{
+		j = i;
+		while (j < size)
+		{
+			_putchar(' ');
+			j++;
+		}
+		j = 1;
+
+		while (j <= i)
+		{
+			_putchar('#');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
