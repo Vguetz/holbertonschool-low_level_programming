3ff41e3a40a56bc32d42252e581dc69681e75d0c
Vguetz <gomezlucas712@gmail.com>
2024-10-29 12:10:18 +0000

    fizzbuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ",i);
+				
+				i++;
+	}
+	return (0);
+}
