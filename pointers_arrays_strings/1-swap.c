edb910a3fbbcf53178bc0d06fceacebfc28e25a8
Vguetz <gomezlucas712@gmail.com>
2024-10-30 12:36:12 +0000

    1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - function to swap numbers
+ * @a: first number to swap
+ * @b: second number to swap
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+	
+
+}
