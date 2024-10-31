260c9de39c7138c1de42e338ca2ee65d405b754a
Vguetz <gomezlucas712@gmail.com>
2024-10-31 15:16:10 +0000

    10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+#include "main.h"
+#include <stdio.h>
+#include <limits.h>
+
+int _atoi(char *s) 
+{
+	int num = 0;
+	int sign = 1;
+	int digit_in_string = 0;
+	int digit;
+
+	while (*s)
+	{
+		if (*s >= '0' && *s <= '9') 
+		{
+			digit_in_string = 1;
+			digit = *s - '0';
+			if (num > (INT_MAX - digit) / 10) 
+			{
+				return (sign == 1) ? INT_MAX : INT_MIN;
+			}
+			num = num * 10 + digit;
+		} else if (*s == '-') 
+		{
+			if (!digit_in_string) 
+			{
+				sign *= -1;
+			}
+		} else if (digit_in_string) 
+		{
+			break;
+		}
+		s++;
+	}
+	return digit_in_string ? sign * num : 0;
+}
+
