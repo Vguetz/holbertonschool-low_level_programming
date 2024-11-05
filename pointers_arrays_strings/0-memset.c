ec8e3c6bb97adf853ed25b985a2fe0f78ad40c8c
Vguetz <gomezlucas712@gmail.com>
2024-11-05 12:42:15 +0000

    0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - function
+ * @s: pointer of memory area
+ * @b: const memory bytes
+ * @n: number of bytes
+ *
+ * Return: a
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+	char *a = 0;
+
+	while (i < n)
+	{
+		s++ = b;
+		i++;
+	}
+	return (a);
+}
