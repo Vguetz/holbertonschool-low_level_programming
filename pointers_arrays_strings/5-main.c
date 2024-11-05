6596cf931f787f0fe7123b21eb4e46d244be36e2
Vguetz <gomezlucas712@gmail.com>
2024-11-05 13:59:25 +0000

    5

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -8,10 +8,11 @@
  */
 int main(void)
 {
-	char s[10] = "My School";
-	
-	printf("%s\n", s);
-	rev_string(s);
-	printf("%s\n", s);
-	return (0);
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
+
+    t = _strstr(s, f);
+    printf("%s\n", t);
+    return (0);
 }
