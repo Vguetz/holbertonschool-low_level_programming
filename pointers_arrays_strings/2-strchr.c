e4aba768437e7b4da58b4a6f4b96fca53faeb6f9
Vguetz <gomezlucas712@gmail.com>
2024-11-05 13:08:10 +0000

    2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strchr - function
+ * @s: string
+ * @c: char
+ *
+ * Return: s + c or null
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + i) == c)
+			return (s + i);
+		i++;
+	}
+	if (*(s + i) == c)
+		return (s + i);
+
+	return NULL;
+}
