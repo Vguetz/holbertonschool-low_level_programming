bf2f20802da3b8f88f3b4099d116a172fe7ba294
Vguetz <gomezlucas712@gmail.com>
2024-11-06 13:33:22 +0000

    2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function
+ * @s: string to count
+ *
+ * Return: string length
+ *
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return ((1 + (_streln_recursion(s + 1))));
+	}
+	return (0);
+}
