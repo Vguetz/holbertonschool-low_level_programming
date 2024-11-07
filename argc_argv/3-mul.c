717328fcbd364a8d77a6d9b5e6cb7b09664cb0ba
Vguetz <gomezlucas712@gmail.com>
2024-11-07 13:25:48 +0000

    2

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int mul = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return(0);
+	}
+
+	mul = atoi(argv[1]) * atoi(argv[2]);
+
+	printf("%d\n", mul);
+	return (mul);
+}
