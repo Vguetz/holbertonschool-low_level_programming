8f428df86b3becaa23970f1622fbfb7c4c4066e0
Vguetz <gomezlucas712@gmail.com>
2024-11-07 12:18:04 +0000

    2

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
-
+/**
+ * main - prints a argv
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: always 0
+ *
+ */
 int main(int argc, char *argv[])
 {
-	(void)argc;
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
 
 }
