48535fb14411c170a29aa8f46844271143f6980f
Vguetz <gomezlucas712@gmail.com>
2024-11-14 14:37:51 +0000

    0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct my_dog - structure
+ * @owner: name of the owner
+ * @name: name of the dog
+ * @age: age of dog
+ *
+ * Description: Structure to create a dog
+ */
+struct my_dog
+{
+	char *owner;
+	char *name;
+	float age;
+};
+#endif
